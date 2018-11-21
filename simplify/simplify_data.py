85e7a5929d75a5d7f4472cfc2528de6057ae6c89
Phill Conrad <pconrad@cs.ucsb.edu>
2018-11-21 10:35:18 -0800

    PC/PH add code to simplify the data format and add simplified data

---
+++ simplify/simplify_data.py
@@ -0,0 +1,21 @@
+
+# Read in from ../data/death.json and write out ../data/compact_death.json
+
+import json
+
+with open('../data/death.json') as json_data:
+    d = json.load(json_data)
+
+
+new_data = []
+
+for item in d['data']:
+    row = {'year': item[8] ,
+           'cause': item[10],
+           'state': item[11],
+           'deaths': item[12],
+           'rate': item[13]}
+    new_data.append(row)
+
+with open('../data/compact_death.json', 'w') as outfile:
+    json.dump(new_data, outfile)
